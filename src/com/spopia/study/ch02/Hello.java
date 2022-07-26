692425a59477675b3457fb0ed564194b16212a9e
HIMMEL <bbluesky7738@gmail.com>
2022-07-26 17:38:23 +0900

    first

---
+++ src/com/spopia/study/ch02/Hello.java
@@ -0,0 +1,47 @@
+package com.spopia.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		// 데이터타입 변수명 = 값;
+		// 데이터타입 변수명
+		
+		boolean abc = true;
+		boolean xyz;
+		xyz = false;
+		
+		int width = 100;
+		String name1 = "이하늘";
+		
+		String name2 = "이하늘";
+		String id = "Himmel";
+		String job = "학생";
+		String dob = "1995.10.08";
+		String email = "himmel@gmail.com";
+		String phone = "010-1234-1234";
+		String gender = "남성";
+		String favoriteTeam = "두산";
+		String password = "123456!@";
+		
+		int zip = 16918;
+		String address = "경기도 용인시 기흘구";
+		String detailAddress = "205동";
+		
+		System.out.println("name 변수에 들어간 값은 : " + name1);
+		System.out.println("name2 변수에 들어간 값은 : " + name2);
+		System.out.println("id 변수에 들어간 값은 : " + id);
+		System.out.println("job 변수에 들어간 값은 : " + job);
+		System.out.println("dob 변수에 들어간 값은 : " + dob);
+		System.out.println("email 변수에 들어간 값은 : " + email);
+		System.out.println("phone 변수에 들어간 값은 : " + phone);
+		System.out.println("gender 변수에 들어간 값은 : " + gender);
+		System.out.println("favoriteTeam 변수에 들어간 값은 : " + favoriteTeam);
+		System.out.println("password 변수에 들어간 값은 : " + password);
+		System.out.println("zip 변수에 들어간 값은 : " + zip);
+		System.out.println("address 변수에 들어간 값은 : " + address);
+		System.out.println("detailAddress 변수에 들어간 값은 : " + detailAddress);
+		
+	}
+
+}
