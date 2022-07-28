84f7101bd65be4e3ae03e6c7c7ac3e1d13c7801c
HIMMEL <bbluesky7738@gmail.com>
2022-07-28 16:44:15 +0900

    first

--- src/com/spopia/study/ch02/Hello.java
+++ src/com/spopia/study/ch02/Hello.java
@@ -6,12 +6,7 @@ public class Hello {
 
 		// 데이터타입 변수명 = 값;
 		// 데이터타입 변수명
-		
-		boolean abc = true;
-		boolean xyz;
-		xyz = false;
-		
-		int width = 100;
+
 		String name1 = "이하늘";
 		
 		String name2 = "이하늘";
