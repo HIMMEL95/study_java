4cb2be45f8bc20b4ae540676a8c0c9d2542d4344
HIMMEL <bbluesky7738@gmail.com>
2022-08-09 15:05:14 +0900

    first

--- src/com/spopia/study/ch04/ifExample3.java
+++ src/com/spopia/study/ch04/ifExample3.java
@@ -8,9 +8,9 @@ public class ifExample3 {
 		int intValue2 = 20;
 		
 		if (intValue1 >  intValue2) {
-			System.out.println("두 변수 중 더 큰 수는 : " + intValue1);
+			System.out.println("두 변수 중 더 큰 수는 intValue1 : " + intValue1);
 		} else if (intValue1 < intValue2) {
-			System.out.println("두 변수 중 더 큰 수는 :" + intValue2);
+			System.out.println("두 변수 중 더 큰 수는 intValue2 :" + intValue2);
 		} else {
 			System.out.println("두 변수는 서로 같은 값입니다.");
 		}
