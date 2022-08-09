e8351d1fc67ac624c41e5769ed089a15899cc10e
HIMMEL <bbluesky7738@gmail.com>
2022-08-09 16:05:43 +0900

    first

--- src/com/spopia/study/ch04/ifExample4.java
+++ src/com/spopia/study/ch04/ifExample4.java
@@ -4,7 +4,12 @@ public class ifExample4 {
 
 	public static void main(String[] args) {
 
+		int intValue = 0;
 		
+		if (intValue % 2 == 0)
+			System.out.println("짝수 입니다.");
+		else 
+			System.out.println("홀수 입니다.");
 	}
 
 }
--- src/com/spopia/study/ch04/ifExample6.java
+++ src/com/spopia/study/ch04/ifExample6.java
@@ -3,8 +3,8 @@ package com.spopia.study.ch04;
 public class ifExample6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 	}
 
 }
