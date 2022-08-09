c084a348ce31125fa6b176c22f296a9255f35b06
HIMMEL <bbluesky7738@gmail.com>
2022-08-09 17:02:21 +0900

    first

--- src/com/spopia/study/ch04/ifExample4.java
+++ src/com/spopia/study/ch04/ifExample4.java
@@ -6,10 +6,18 @@ public class ifExample4 {
 
 		int intValue = 0;
 		
-		if (intValue % 2 == 0)
-			System.out.println("짝수 입니다.");
-		else 
-			System.out.println("홀수 입니다.");
+//		if (intValue % 2 == 0)
+//			System.out.println("짝수 입니다.");
+//		else 
+//			System.out.println("홀수 입니다.");
+		
+		if (intValue != 0) {
+			if (intValue % 2 == 0)
+				System.out.println("짝수 입니다.");
+			else 
+				System.out.println("홀수 입니다.");
+		} else 
+			System.out.println("0은 홀짝을 판단할 수 없습니다.");
 	}
 
 }
--- src/com/spopia/study/ch04/ifExample6.java
+++ src/com/spopia/study/ch04/ifExample6.java
@@ -4,7 +4,80 @@ public class ifExample6 {
 
 	public static void main(String[] args) {
 
+		int num1 = 7, num2 = 7, num3 = 7;
+		
+		if (num1 != num2 && num2 != num3 && num3 != num1) {
+			if (num1 > num2 && num1 > num3)
+				System.out.println("최댓값은 num1 : " + num1);
+			else if (num2 > num1 && num2 > num3)
+				System.out.println("최댓값은 num2 : " + num2);
+			else
+				System.out.println("최댓값은 num3 : " + num3);
+			
+			if (num1 < num2 && num1 < num3)
+				System.out.println("최솟값은 num1 : " + num1);
+			else if (num2 < num1 && num2 < num3)
+				System.out.println("최솟값은 num2 : " + num2);
+			else
+				System.out.println("최솟값은 num3 : " + num3);
+		} else 
+			System.out.println("적어도 2개 이상의 동일한 정수가 존재 합니다. 수정해 주세요.");
+		
+		
+//		compare_max(num1, num2, num3);
+//		compare_min(num1, num2, num3);
 		
 	}
+	
+//	static void compare_max(int num1, int num2, int num3) {
+//		
+//		int max = 0;
+//		
+//		if (num1 < num2) {
+//			if (num2 < num3)
+//				max = num3;
+//			else
+//				max = num2;
+//		} else if (num1 > num2){
+//			if (num1 > num3)
+//				max = num1;
+//			else
+//				max = num3;
+//		} else {
+//			if (num1 == num3)
+//				max = num1;
+//			else if (num1 > num3)
+//				max = num1;
+//			else
+//				max = num3;				
+//		}
+//		 System.out.println("최댓값은 " + max  + "입니다.");
+//	}
+//	
+//	static void compare_min(int num1, int num2, int num3) {
+//		
+//		int min = 0;
+//		
+//		if (num1 < num2) {
+//			if (num1 < num3)
+//				min = num1;
+//			else
+//				min = num3;
+//		}
+//		else if (num1 > num2){
+//			if (num2 > num3)
+//				min = num3;
+//			else
+//				min = num2;
+//		} else {
+//			if (num1 > num3)
+//				min = num3;
+//			else if (num1 < num3)
+//				min = num1;
+//			else
+//				min = num1;
+//		}
+//		 System.out.println("최솟값은 " + min  + "입니다.");
+//	}
 
 }
