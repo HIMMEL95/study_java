13622d6a166f858e898897645afb4300c2f22e2e
HIMMEL <bbluesky7738@gmail.com>
2022-08-09 14:50:37 +0900

    first

--- src/com/spopia/study/ch04/ifExample2.java
+++ src/com/spopia/study/ch04/ifExample2.java
@@ -8,23 +8,23 @@ public class ifExample2 {
 		
 		if (grade == 1) {
 			price *= 0.95;
-			System.out.println("5% 할인적용가 : " + price);
+			System.out.println("일반 등급 5% 할인적용가 : " + price);
 		}
 		else if (grade == 2) {
 			price *= 0.9;
-			System.out.println("10% 할인적용가 : " + price);
+			System.out.println("브론즈 등급 10% 할인적용가 : " + price);
 		}
 		else if (grade == 3) {
 			price *= 0.85;
-			System.out.println("15% 할인적용가 : " + price);
+			System.out.println("실버 등급 15% 할인적용가 : " + price);
 		}
 		else if (grade == 4) {
 			price *= 0.8;
-			System.out.println("20% 할인적용가 : " + price);
+			System.out.println("골드 등급 20% 할인적용가 : " + price);
 		}
 		else {
 			price *= 0.973;
-			System.out.println("2.7% 할인적용가 : " + price);
+			System.out.println("플래티넘 등급 2.7% 할인적용가 : " + price);
 		}
 	}
 
---
+++ src/com/spopia/study/ch04/ifExample3.java
@@ -0,0 +1,10 @@
+package com.spopia.study.ch04;
+
+public class ifExample3 {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
