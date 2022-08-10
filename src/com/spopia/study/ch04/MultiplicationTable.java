8b3c1c9457473336bfd170e1687e7b169e121d74
HIMMEL <bbluesky7738@gmail.com>
2022-08-10 15:22:08 +0900

    first

--- src/com/spopia/study/ch04/MultiplicationTable.java
+++ src/com/spopia/study/ch04/MultiplicationTable.java
@@ -17,7 +17,7 @@ public class MultiplicationTable {
 		for (int i = 2; i < 10; i++) {
 			System.out.println(i + "단");
 			for(int j = 1; j < 10; j++) {
-				System.out.println(i + " * " + j + " = " + (i  * j));
+				System.out.println(i + " * " + j + " = " + (i * j));
 			}
 			System.out.println();
 		}
