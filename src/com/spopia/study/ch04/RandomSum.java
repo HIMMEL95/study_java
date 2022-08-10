a6debf3836ac78ed70febb66f0a5dc1be4d92a4b
HIMMEL <bbluesky7738@gmail.com>
2022-08-10 15:58:34 +0900

    first

--- src/com/spopia/study/ch04/RandomSum.java
+++ src/com/spopia/study/ch04/RandomSum.java
@@ -7,15 +7,13 @@ public class RandomSum {
 		// 1 ~ 100 사이의 랜덤 정수 2개를 발생시켜
 		// 두 수 사이의 합을 구하라
 		
-//		double num1 = 1.0;
-//		double num2 = 100.0;
-//		
-		int sum = 0, min = 0, max = 0;
-//		
-//		int random1 = (int) ((Math.random() * (num2 - num1)) + num1);
-//		int random2 = (int) ((Math.random() * (num2 - num1)) + num1);
+		double num1 = 1.0;
+		double num2 = 100.0;
 		
-		int random1 = 10, random2 = 10;
+		int sum = 0, min = 0, max = 0;
+		
+		int random1 = (int) ((Math.random() * (num2 - num1)) + num1);
+		int random2 = (int) ((Math.random() * (num2 - num1)) + num1);
 		
 		if (random1 < random2) {
 			max = random2;
