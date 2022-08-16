fa6893cf63c39970d45cf8f2832430b7015def3b
HIMMEL <bbluesky7738@gmail.com>
2022-08-16 17:29:31 +0900

    first

--- src/com/spopia/study/ch04/ForExample.java
+++ src/com/spopia/study/ch04/ForExample.java
@@ -15,15 +15,14 @@ public class ForExample {
 		
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 10; j++) {
-				if (i == 2 || i == 3 || i == 4) {
+				if (i == 1 || i == 5) {
+					System.out.print("*");
+				} else {
 					if (j == 1 || j == 10) {
 						System.out.print("*");
-					}
-					else {
+					} else {
 						System.out.print(" ");
 					}
-				} else {
-					System.out.print("*");
 				}
 			}
 			System.out.println();
