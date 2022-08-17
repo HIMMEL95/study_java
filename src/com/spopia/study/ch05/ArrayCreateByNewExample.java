74853d5f451a0ffb474855d7bde6b0e76accedbf
HIMMEL <bbluesky7738@gmail.com>
2022-08-17 17:06:53 +0900

    first

---
+++ src/com/spopia/study/ch05/ArrayCreateByNewExample.java
@@ -0,0 +1,20 @@
+package com.spopia.study.ch05;
+
+public class ArrayCreateByNewExample {
+
+	public static void main(String[] args) {
+
+		int[] arr1 = new int[3];
+		
+		int[] arr2;
+		arr2 = new int[5];
+		
+		arr1[0] = 77;
+		arr1[1] = 50;
+		arr1[2] = 34;
+		System.out.println("arr1[0] : " + arr1[0]);
+		System.out.println("arr1[1] : " + arr1[1]);
+		System.out.println("arr1[2] : " + arr1[2]);
+	}
+
+}
