9ea9e5951862794b1afd541add440c6ccca31594
HIMMEL <bbluesky7738@gmail.com>
2022-08-17 16:25:23 +0900

    first

---
+++ src/com/spopia/study/ch05/ArrayCreateByValueListExample1.java
@@ -0,0 +1,29 @@
+package com.spopia.study.ch05;
+
+
+public class ArrayCreateByValueListExample1 {
+
+	public static void main(String[] args) {
+
+		int[] scoreEnglish;
+		int scoreMath[];
+		
+		int[] scoreChemistry = {88, 99, 45, 86, 34};
+		
+		String[] name = {"장원영", "장동건", "고소영"};
+		
+		System.out.println("명지님 화학성적 : " + scoreChemistry[0]);
+		
+		System.out.println("누굴까 : " + name[1]);
+		
+		double sum = 0, avg = 0;
+		
+		for (int i=0; i<=4; i++) {
+			sum += scoreChemistry[i];
+		}
+		
+		avg = sum / 5;
+		
+		System.out.println("총점은 : " + sum + ", 평점은 : " + avg);
+	}
+}
