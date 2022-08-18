0a432f12b8d2d12e697e495e5ab9010a79bceb3b
HIMMEL <bbluesky7738@gmail.com>
2022-08-18 16:21:12 +0900

    first

---
+++ src/com/spopia/study/ch05/Week.java
@@ -0,0 +1,15 @@
+package com.spopia.study.ch05;
+
+public enum Week { // Enumberation Type
+	
+	// Enumberation constants: 열거 상수
+	
+	MONDAY,
+	THUESDAY,
+	WEDNESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+
+}
