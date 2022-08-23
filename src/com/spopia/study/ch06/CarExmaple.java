df76dd2a9aa5e87dc85647a0376bb42b6744df14
HIMMEL <bbluesky7738@gmailc.om>
2022-08-23 15:03:43 +0900

    first

--- src/com/spopia/study/ch06/CarExmaple.java
+++ src/com/spopia/study/ch06/CarExmaple.java
@@ -5,10 +5,10 @@ public class CarExmaple {
 	public static void main(String[] args) {
  
 		Car abc = new Car();
-		int a = 1;
-		int c = 1;
-		
-		int[] b = new int[1];
+//		int a = 1;
+//		int c = 1;
+//		
+//		int[] b = new int[1];
 		
 		System.out.println("car.name : " + abc.name);
 		System.out.println("car.name2[1] : " + abc.name2[1]);
