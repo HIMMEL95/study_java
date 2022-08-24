bed1c8a47537d9b4b23b760b27e59cb37f000a03
HIMMEL <bbluesky7738@gmailc.om>
2022-08-24 15:16:02 +0900

    first

---
+++ src/com/spopia/study/ch06/Article.java
@@ -0,0 +1,36 @@
+package com.spopia.study.ch06;
+
+public class Article {
+
+	String name = "손흥민";
+	int count = 1;
+	boolean useNY = false;
+	
+	Article() {}
+
+	Article(String name) {
+		this.name = name;
+	}
+	
+	Article(String name, int count) {
+		this.name = name;
+		this.count = count;
+	}
+	
+	Article(String name, boolean useNY) {
+		this.name = name;
+		this.useNY = useNY;
+	}
+	
+	Article(String name, int count, boolean useNY) {
+		this.name = name;
+		this.count = count;
+		this.useNY = useNY;
+	}
+	
+	void print() {
+		System.out.println("name : " + name + "count : "+ count + "useNY : " + useNY);
+	}
+	
+	static String nationality = "대한민국";
+}
---
+++ src/com/spopia/study/ch06/ArticleExample.java
@@ -0,0 +1,33 @@
+package com.spopia.study.ch06;
+
+public class ArticleExample {
+
+	public static void main(String[] args) {
+
+		Article article = new Article();
+		article.print();
+		
+		System.out.println("------------------------------------------");
+		
+		Article article2 = new Article("이하늘");
+		article2.print();
+		
+		System.out.println("------------------------------------------");
+		
+		Article article3 = new Article("박지성", 10);
+		article3.print();
+		
+		System.out.println("------------------------------------------");
+		
+		Article article4 = new Article("박태환", true);
+		article4.print();
+		
+		System.out.println("------------------------------------------");
+		
+		Article article5 = new Article("이승엽", 20 ,true);
+		article5.print();
+		
+		System.out.println(Article.nationality);
+	}
+
+}
