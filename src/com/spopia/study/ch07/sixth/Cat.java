b49d50d2626eb7fcfc04ec1c3dca4d1fb20e01db
HIMMEL <bbluesky7738@gmail.com>
2022-08-29 17:23:08 +0900

    first

---
+++ src/com/spopia/study/ch07/sixth/Cat.java
@@ -0,0 +1,18 @@
+package com.spopia.study.ch07.sixth;
+
+public class Cat extends Animal{
+
+	public Cat() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("야옹");
+	}
+	
+	@Override
+	public void sound(String aaa) {
+		System.out.println("야옹 : " + aaa);
+	}
+}
