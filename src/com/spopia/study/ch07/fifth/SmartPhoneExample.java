a16339ee88d795db6d4903290d69dc2533064378
HIMMEL <bbluesky7738@gmail.com>
2022-08-29 17:00:34 +0900

    first

---
+++ src/com/spopia/study/ch07/fifth/SmartPhoneExample.java
@@ -0,0 +1,16 @@
+package com.spopia.study.ch07.fifth;
+
+public class SmartPhoneExample {
+
+	public static void main(String[] args) {
+
+//		Phone phone = new Phone();
+		
+		SmartPhone smartPhone = new SmartPhone("asdf");
+		
+		smartPhone.internetSearch();
+		smartPhone.turnOff();
+		smartPhone.turnOn();
+	}
+
+}
